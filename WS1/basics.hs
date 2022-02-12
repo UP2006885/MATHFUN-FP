cd6cf6e7681f28e52d01f0e68988195f8429be6f
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-12 10:40:30 +0000

    Completed WS 1, added answer to question 9 & 10.

--- WS1/basics.hs
+++ WS1/basics.hs
@@ -24,3 +24,7 @@ isEven :: Int -> Bool
 isEven x = (divisibleBy x 2)
 
 averageOfThree :: Int -> Int -> Int -> Float
+averageOfThree x y z = (fromIntegral(x+y+z)/3)
+
+absolute :: Int -> Int
+absolute x = if(x > 0) then x else (x * (-1))
