af3934bd38292ae5ce9c58dda5e79963f154db70
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-12 00:21:29 +0000

    Added answers for questions 3, 4 & 5.

--- WS1/basics.hs
+++ WS1/basics.hs
@@ -3,3 +3,13 @@ timesTen x = 10 * x
 
 sumThree :: Int -> Int -> Int -> Int
 sumThree a b c = a + b + c
+
+
+areaOfCircle :: Float -> Float
+areaOfCircle x = pi * (x ^ 2)
+
+volumeOfCylinder :: Float -> Float -> Float
+volumeOfCylinder r l = (areaOfCircle r) * l
+
+distance :: Float -> Float -> Float -> Float -> Float
+distance x1 y1 x2 y2 = sqrt(((y1 - y2)^2) + ((x1 - x2)^2))
