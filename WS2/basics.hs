909db0e80468b230de0f6091192c992937914371
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-16 22:05:24 +0000

    Finished Ws 2

--- WS2/basics.hs
+++ WS2/basics.hs
@@ -15,19 +15,41 @@ howManyFunctions x y z
     | x == y || y == z || z == x = 2
     | otherwise = 0
 
--- Come back to this one.
--- sumDiagonalLengths :: Float -> Float -> Float -> Float
--- sumDiagonalLengths x y z
+sumDiagonalLengths :: Float -> Float -> Float -> Float
+sumDiagonalLengths x y z = ((x*rootTwo) + (y*rootTwo) + (z*rootTwo))
+    where 
+    rootTwo = 1.41421356237
 
--- 2.20
--- 50p Per Kil for first 10
--- 30p Per Kil for additional.
--- Guards
-gTaxiFairs :: Float -> Float
-gTaxiFairs x
-    | x == 1 = 2.20
-    | x > 1 && x < 11 = (2.20 + ((x-1) * 0.50))
-    | otherwise = (2.20 + (9 * 0.50) + ((x - 10) * 0.30))
+taxiFair :: Int -> Float
+taxiFair x
+    | x < 1 = defaultPrice
+    | x < 11 = defaultPrice + (fromIntegral(x) * first10Kms)
+    | otherwise = defaultPrice + (5) + ((fromIntegral(x) - 10) * additionalKms)
+    where 
+    defaultPrice = 2.20
+    first10Kms = 0.50
+    additionalKms = 0.30
 
--- Where
--- taxiFairs :: Int -> Float
+howManyAboveAverage :: Int -> Int -> Int -> Int
+howManyAboveAverage x y z
+    | (x > average && y > average && z > average) = 3
+    | ((x > average && y > average) || (z > average && y > average) || (z > average && x > average)) = 2
+    | (x > average || y > average || z > average) = 1
+    |  otherwise = 0
+    where
+    average = round(fromIntegral(x+y+z)/3)
+
+validDate :: Int -> Int -> Bool
+validDate x y
+    | ((x < 1 || y < 1) || (x > 31 || y > 12)) = False
+    | (y == 2 && x > 28) = False
+    | ((y == 4 || y ==  6 || y == 9 || y == 11) && x > 30) = False
+    | otherwise = True
+
+daysInMonth :: Int -> Int -> Int
+daysInMonth x y
+    | (x > 12 || x < 1) = 0
+    | ((y `mod` 4) == 0 && x == 2) = 29
+    | x == 2 = 28
+    | x == 4 || x == 6 || x == 9 || x == 11 = 30
+    | otherwise = 31
