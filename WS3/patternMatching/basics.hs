b44f5612b5bc10dea36497c43c983392122d3777
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-02-21 12:42:35 +0000

    Basic pattern matching

---
+++ WS3/patternMatching/basics.hs
@@ -0,0 +1,15 @@
+import Prelude hiding ((||)) -- Explicity says we are importing the prelude, but removing the or boolean function from the import.
+
+(||) :: Bool -> Bool -> Bool
+True || True    = True
+False || True   = True
+True || False   = True
+False || False  = False
+
+(||) :: Bool -> Bool -> Bool 
+False || False  = False
+_ || _          = True
+
+(||) :: Bool -> Bool -> Bool
+True || _ = True
+False || p = p
