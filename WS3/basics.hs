d078ac4801e6767edf09b2f75bfd3b4db98e8ef7
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-04-17 15:11:19 +0100

    Finished off Ws3 without answer for findRoot

--- WS3/basics.hs
+++ WS3/basics.hs
@@ -1,7 +1,7 @@
 -- We don't import '||' from the prelude, so that we can 
 -- define our own version
 
-import Prelude hiding ((||), (&&)) 
+import Prelude hiding ((||), (&&), gcd) 
 
 -- The following line declares the || operator (which we are about to
 -- re-define) to be right associative and to have precedence 2. This
@@ -70,6 +70,48 @@ exOr a False    = a
 exOr _  _        = False
 
 ifThenElse :: Bool -> Int -> Int -> Int
-ifThenElse a b c
-ifThenElse (a == True) = b
-ifThenElse (a == False) = c
+ifThenElse True x y    = x
+ifThenElse False x y   = y
+
+daysInMonth :: Int -> Int
+daysInMonth 2 = 28
+daysInMonth 4 = 20
+daysInMonth 6 = 20
+daysInMonth 9 = 20
+daysInMonth 11 = 20
+daysInMonth m = if (m > 12 || m < 0) then 0 else 31
+
+validDateChecker:: Int -> Bool
+validDateChecker m = if daysInMonth m == 0 then False else True
+
+-- Recursion
+
+sumNumbers :: Int -> Int
+sumNumbers n
+    | n == 0    = 0
+    | n > 0     = sumNumbers(n - 1) + n
+
+sumSquares :: Int -> Int
+sumSquares n
+    | n == 0 = 0
+    | n > 0 = sumSquares(n - 1) + n^2
+
+power :: Int -> Int -> Int
+power z x
+    | x == 0 = 1
+    | x > 0 = z * power z (x-1)
+
+sumFromTo :: Int -> Int -> Int
+sumFromTo x y
+    | x > y = 0
+    | x <= y = sumFromTo(x+1) y + x
+
+gcd :: Int -> Int -> Int
+gcd x y
+    | y == 0 = abs x
+    | otherwise = gcd y (mod x y)
+
+intSquareRoot :: Int -> Int
+intSquareRoot n = findRoot n n
+
+-- Find Root Missing.
