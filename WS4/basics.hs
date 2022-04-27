57dc7ce3b370f555a1a23b9f7d0c24e82fcda007
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-04-27 14:26:21 +0100

    Updated WS4

--- WS4/basics.hs
+++ WS4/basics.hs
@@ -29,5 +29,66 @@ minAndMax x y
 
 -- My Code.
 
+-- Tuples
 sumDifference :: Int -> Int -> (Int,Int)
+sumDifference x y = (x+y, x-y)
 
+grade :: StudentMark -> Char
+grade (s1, m1)
+    | m1 >= 70 = ('A')
+    | m1 >= 60 = ('B')
+    | m1 >= 50 = ('C')
+    | m1 >= 40 = ('D')
+    | otherwise = ('F')
+
+grading :: StudentMark -> (String, Char)
+grading (s1, m1)
+    | m1 >= 70 = (s1, 'A')
+    | m1 >= 60 = (s1, 'B')
+    | m1 >= 50 = (s1, 'C')
+    | m1 >= 40 = (s1, 'D')
+    | otherwise = (s1, 'F')
+
+capMark :: StudentMark -> StudentMark
+capMark (s1, m1)
+    | m1 > 40 = (s1, 40)
+    | otherwise = (s1, m1)
+
+-- Lists and Strings
+firstNumbers :: Int -> [Int]
+firstNumbers n 
+    | n == 1 = [n]
+    | otherwise = firstNumbers(n-1) ++ list
+    where
+    list = [n]
+
+firstSquares :: Int -> [Int]
+firstSquares n
+    | n == 0 = []
+    | otherwise = firstSquares(n-1) ++ list
+    where
+    list = [n^2]
+
+--      List Comprehensions
+capitalise :: String -> String
+capitalise string = [ toUpper s | s <- string ]
+
+onlyDigits :: String -> String
+onlyDigits string = [ s | s <- string, not $ isAlpha s ]
+
+capMarks :: [StudentMark] -> [StudentMark]
+capMarks students = [capMark student | student <- students]
+
+gradeStudents :: [StudentMark] -> [(String,Char)]
+gradeStudents students = [grading m1 | m1 <- students]
+
+-- duplicate ICBA
+
+divisors :: Int -> [Int]
+divisors n = [ x | x <- [1..(n-1)], n `rem` x == 0]
+
+isPrime :: Int -> Bool
+isPrime x
+    | length (divisors(x)) <= 2 = True
+    | otherwise = False
+    
